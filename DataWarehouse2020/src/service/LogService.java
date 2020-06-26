d60862544815ab68365f00bcb115f96cde4d4c21
Phuong <thuyphuong261299@gmail.com>
2020-06-26 21:21:15 +0700

    change

--- DataWarehouse2020/src/service/LogService.java
+++ DataWarehouse2020/src/service/LogService.java
@@ -1,10 +1,12 @@
 package service;
 
+import java.sql.SQLException;
+
 import model.MyFile;
 
 public interface LogService {
 	
-	public boolean insertLog(int configID, String fileName, String fileType, String status, String fileTimeStamp);
-	public MyFile getFileWithStatus(String status);
+	public boolean insertLog(int configID, String fileName, String fileType, String status, String fileTimeStamp) throws SQLException;
+	public MyFile getFileWithStatus(String status) throws SQLException;
 
 }
