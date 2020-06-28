fa5b29e9f5c25934f0dd04accca1d38fd10c824b
Phuong <thuyphuong261299@gmail.com>
2020-06-28 18:51:59 +0700

    LOAD DATA LOCAL INFILE statement

--- DataWarehouse2020/src/service/DBService.java
+++ DataWarehouse2020/src/service/DBService.java
@@ -5,8 +5,9 @@ import java.sql.SQLException;
 public interface DBService {
 	
 	public boolean existTable(String table_name) throws SQLException;
-	public boolean insertValues(String target_table, String column_list, String values) throws SQLException;
-	public boolean createTable(String table_name, String variables, String column_list) throws SQLException;
-	public boolean deleteTable(String table_name) throws SQLException;
+	public int insertValues(String target_table, String column_list, String values) throws SQLException;
+	public int createTable(String table_name, String variables, String column_list) throws SQLException;
+	public int deleteTable(String table_name) throws SQLException;
+	public int loadFile(String sourceFile, String tableName, String dilimiter) throws SQLException;
 
 }
--- DataWarehouse2020/src/service/DBServiceImpl.java
+++ DataWarehouse2020/src/service/DBServiceImpl.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import db.DBConnection;
+import model.Configuration;
 
 public class DBServiceImpl implements DBService {
 	String targetDBName;
@@ -26,39 +27,64 @@ public class DBServiceImpl implements DBService {
 	}
 
 	@Override
-	public boolean insertValues(String target_table, String column_list, String values) throws SQLException {
+	public int insertValues(String target_table, String column_list, String values) throws SQLException {
 		Connection connection;
 		connection = DBConnection.getConnection(targetDBName);
 		PreparedStatement ps = connection
 				.prepareStatement("INSERT INTO " + target_table + " (" + column_list + ") VALUES " + values);
 		System.out.println("INSERT INTO " + target_table + " (" + column_list + ") VALUES " + values);
-		ps.executeUpdate();
-		connection.close();
-		return true;
+		return ps.executeUpdate();
 	}
 
 	@Override
-	public boolean createTable(String table_name, String variables, String column_list) throws SQLException {
+	public int createTable(String table_name, String variables, String column_list) throws SQLException {
 		String sql = "CREATE TABLE " + table_name + " (";
 		String[] vari = variables.split(",");
 		String[] col = column_list.split(",");
 		for (int i = 0; i < vari.length; i++) {
-			sql += col[i] + " " + vari[i] + " NOT NULL,";
+			sql += col[i] + " " + vari[i] + " ,";
 		}
 		sql = sql.substring(0, sql.length() - 1) + ")";
 		System.out.println(sql);
 		Connection connection;
 		connection = DBConnection.getConnection(targetDBName);
 		PreparedStatement ps = connection.prepareStatement(sql);
-		ps.executeUpdate();
-		connection.close();
-		return true;
+		return ps.executeUpdate();
 	}
 
 	@Override
-	public boolean deleteTable(String table_name) throws SQLException {
+	public int deleteTable(String table_name) throws SQLException {
 		// TODO Auto-generated method stub
-		return false;
+		return 0;
+	}
+	
+	//LOAD DATA LOCAL INFILE '/path/pet.txt' INTO TABLE pet;
+	@Override
+	public int loadFile(String sourceFile, String tableName, String dilimiter) throws SQLException {
+		Connection connection;
+		connection = DBConnection.getConnection(targetDBName);
+		sourceFile = sourceFile.replace("\\", "\\\\");
+		PreparedStatement ps = connection.prepareStatement("LOAD DATA LOCAL INFILE '"+sourceFile+"' INTO TABLE "+tableName+"\r\n" + 
+															"FIELDS TERMINATED BY '"+dilimiter+"' \r\n" + 
+															"ENCLOSED BY '\"' \r\n" + 
+															"LINES TERMINATED BY '\\r\\n'");
+		System.out.println("LOAD DATA LOCAL INFILE '"+sourceFile+"' INTO TABLE "+tableName+"\r\n" + 
+				"FIELDS TERMINATED BY '"+dilimiter+"' \r\n" + 
+				"ENCLOSED BY '\"' \r\n" + 
+				"LINES TERMINATED BY '\\r\\n'");
+		return ps.executeUpdate();
+	}
+	
+	public static void main(String[] args) {
+		Configuration config = new Configuration("sinhvien");
+		DBService test = new DBServiceImpl("staging");
+		try {
+//			System.out.println(test.createTable("sinhvien", config.getVariabless(), config.getFileColumnList()));
+			System.out.println(test.loadFile("local\\test\\data_1999-12-10_018.txt", "sinhvien","|"));
+		} catch (SQLException e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}
 	}
 
 }
