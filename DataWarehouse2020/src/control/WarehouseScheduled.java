5e5f161761962da2eb15405e1f09859d14344753
Tien <17130242@st.hcmuaf.edu.vn>
2020-08-10 10:20:33 +0700

    comment

--- DataWarehouse2020/src/control/WarehouseScheduled.java
+++ DataWarehouse2020/src/control/WarehouseScheduled.java
@@ -33,37 +33,46 @@ public class WarehouseScheduled {
 		Configuration config;
 		boolean success = false;
 		try {
+//1. Kết nối vào database control và lấy ra config_id có flag = "prepare"
 			int configID = dbControl.getFlag("prepare");
 			System.out.println(configID);
 			if (configID != 0) {
 				config = new Configuration(configID, "root", "1234");
 				
+//2. Thay đổi thay đổi trạng thái của cờ = "Running step 1"
 				//Step 1
 				dbControl.updateFlag(configID, "Running Step 1");
+//3. Tiến hành down load file
 				Download step1  = new Download(config);
 				success = step1.DownloadFile();
 				if(!success) 
 					return;
 				
-				dbControl.updateFlag(configID, "Done Step 1");
+//				dbControl.updateFlag(configID, "Done Step 1");
 				
+//4. Thay đổi thay đổi trạng thái của cờ = "Running step 2"
 				//Step 2
 				dbControl.updateFlag(configID, "Running Step 2");
 				dbStaging.truncateTable(config.getConfigName());
+//5.Tiến hành truncate staging và load file vào staging và transform
 				ExtractToStaging step2 = new ExtractToStaging(config, fileService, dbStaging, logService);
 				success = step2.extractToStaging();
 				if(!success) 
 					return;
-				dbControl.updateFlag(configID, "Done Step 2");
+//				dbControl.updateFlag(configID, "Done Step 2");
 				
+//6.Thay đổi thay đổi trạng thái của cờ = "Running step 2"
 				//Step 3
 				dbControl.updateFlag(configID, "Running Step 3");
+//7.Tiến hành load file từ staging và warehouse
 				LoadToWarehouse step3 = new LoadToWarehouse(config, fileService, dbWarehouse, logService);
 				success = step3.loadToWarehouse();
 				if(!success) 
 					return;
+//8.Thay đổi thay đổi trạng thái của cờ = "Done Step 3"
 				dbControl.updateFlag(configID, "Done Step 3");
 				
+//9.Thay đổi thay đổi flag của config kế tiếp thành "prepare"
 				//next config
 				configID++;
 				int nextConfig = dbControl.nextConfig(configID);
